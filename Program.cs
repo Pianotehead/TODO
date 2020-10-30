fcf0079066b326f89bd1913a2d60e33f07227757
Theodor Nordkvist <teddin2@outlook.com>
2020-10-30 15:54:47 +0100

    List tasks from database

--- Program.cs
+++ Program.cs
@@ -38,7 +38,8 @@ namespace TODO
                         break;
                     case ConsoleKey.D2:
                     case ConsoleKey.NumPad2:
-                        int[] columnWidths = { 50, 20 };
+                        int[] columnWidths = { 4, 50, 20, 20 };
+                        var myTaskList = FetchMyTasks();
                         MyTask.ListTasks(myTaskList, columnWidths);
                         ConsoleKey deleteOrNot = Menu.ActOnOnlyTheseKeys(ConsoleKey.D, ConsoleKey.Escape);
                         break;
@@ -51,7 +52,7 @@ namespace TODO
             Clear();
             WriteLine("\n  Thank you for using this To Do application");
             Thread.Sleep(2000);
-
+            CursorVisible = true;
         }
 
         private static void AddTask()
@@ -109,7 +110,82 @@ namespace TODO
             connection.Open();
             command.ExecuteNonQuery();
             connection.Close();
-        }  
+        }
+
+        private static IList<MyTask> FetchMyTasks()
+        {
+            string sql = "SELECT Id, Name, DueDate, CompletedAt FROM MyTask";
+
+            List<MyTask> myTaskList = new List<MyTask>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var id = (int)reader["Id"];
+                    var name = (string)reader["Name"];
+                    string dueDateAsString = reader["DueDate"].ToString();
+                    string completionDate = reader["CompletedAt"].ToString();
+                    if (string.IsNullOrWhiteSpace(dueDateAsString) && string.IsNullOrWhiteSpace(completionDate))
+                    {
+                        myTaskList.Add(new MyTask(id, name, null, null));
+                    }
+                    else if (string.IsNullOrWhiteSpace(dueDateAsString) && !string.IsNullOrWhiteSpace(completionDate))
+                    {
+                        var completedAt = (DateTime)reader["CompletedAt"];
+                        myTaskList.Add(new MyTask(id, name, null, completedAt));
+                    }
+                    else if (!string.IsNullOrWhiteSpace(dueDateAsString) && string.IsNullOrWhiteSpace(completionDate))
+                    {
+                        var dueDate = (DateTime)reader["DueDate"];
+                        myTaskList.Add(new MyTask(id, name, dueDate, null));
+                    }
+                    else
+                    {
+                        var dueDate = (DateTime)reader["CompletedAt"];
+                        var completedAt = (DateTime)reader["CompletedAt"];
+                        myTaskList.Add(new MyTask(id, name, dueDate, completedAt));
+                    }
+                }
+
+                connection.Close();
+            }
+            return myTaskList;
+        }
+
+        private static void ListTasks()
+        {
+            var myTaskList = FetchMyTasks();
+
+            Console.WriteLine($"{"Name",-50}Due Date");
+            Console.WriteLine("============================================================");
+
+            foreach (var myTask in myTaskList)
+            {
+                Console.WriteLine($"{myTask.Name,-50}{(myTask.DueDate.HasValue ? myTask.DueDate.Value.ToString() : "")}");
+            }
+
+            Console.CursorTop++;
+            Console.WriteLine("[D] Delete | [Esc] Return");
+            ConsoleKeyInfo input;
+
+            while (
+                (input = Console.ReadKey(true)).Key != ConsoleKey.D &&
+                input.Key != ConsoleKey.Escape)
+            { }
+
+            // if (input.Key == ConsoleKey.D)
+            // {
+            //     RemoveTask(myTaskList);
+            // }
+            Console.Clear();
+        }
+
 
         public static DateTime ConvertSuccessfullyToDate(string dateAsText)
         {
